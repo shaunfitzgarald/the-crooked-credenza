4acb5db4adf62ad23de129d3c739e1fd7a537908
Shaun Stephenson <121666573+shaunfitzgarald@users.noreply.github.com>
2024-06-17 13:00:33 -0700

    Created individual image detail pages that are linked to each image on index.html.

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
     links.forEach(link => {
         link.addEventListener('click', (e) => {
             const href = link.getAttribute('href');
-            if (href.startsWith('index.html#')) {
+            if (href.startsWith('index.html#') || href.startsWith('../index.html#')) {
                 // Handle navigation to index.html sections
                 e.preventDefault();
                 window.location.href = href;
-            } else if (href === 'index.html') {
+            } else if (href === 'index.html' || href === '../index.html') {
                 // Handle scroll to top of index.html
                 e.preventDefault();
-                if (window.location.pathname !== '/index.html') {
-                    window.location.href = 'index.html';
+                if (window.location.pathname !== '/index.html' && window.location.pathname !== '/your-subdirectory-path/index.html') {
+                    window.location.href = href;
                 } else {
                     window.scrollTo({
                         top: 0,
